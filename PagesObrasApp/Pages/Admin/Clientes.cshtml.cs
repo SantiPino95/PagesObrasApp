ed154c52820fcd7d9fd874bb6729c39dff45c5e3
josegomez <josegabrielgomez2005@gmail.com>
2026-06-22 20:05:11 -0300

    Agrego 3 htmls mas y rediseño del AdminLayout

--- PagesObrasApp/Pages/Admin/Clientes.cshtml.cs
+++ PagesObrasApp/Pages/Admin/Clientes.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PagesObrasApp.Pages.Admin
 {
-    public class ProveedorModel : PageModel
+    public class ClientesModel : PageModel
     {
         public void OnGet()
         {
